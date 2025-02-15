4ac523b039e69161640cd43e9c72ca24c598c530
YianXie <yian.xie@outlook.com>
2025-02-15 11:35:37 +0800

    update

--- home/static/home.js
+++ home/static/home.js
@@ -6,23 +6,25 @@ document.addEventListener("DOMContentLoaded", async function () {
         // Append the text
         for (let l = 0; l < roles[c].length; l++) {
             role.innerHTML += roles[c][l];
-            role.style.opacity = (1 / roles[c].length * l);
-            await sleep(50);
+            // role.style.opacity = (1 / roles[c].length * l);
+            await sleep(75);
         }
 
         // Wait for a while
-        await sleep(500);
+        await sleep(750);
 
         // Remove the text
         while (role.innerHTML.length > 0) {
             role.innerHTML = role.innerHTML.slice(0, role.innerHTML.length - 1);
-            role.style.opacity = (1 / roles[c].length * role.innerHTML.length);
-            await sleep(50);
+            // role.style.opacity = (1 / roles[c].length * role.innerHTML.length);
+            await sleep(40);
         }
 
         // Infinite loop
-        if (c === roles.length - 1)
+        if (c === roles.length - 1) {
             c = 0;
+            await sleep(500);
+        }
     }
 });
 
