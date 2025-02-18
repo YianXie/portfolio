91d606371d659390d6e630af97dc62b0b840eca5
YianXie <yian.xie@outlook.com>
2025-02-18 16:58:55 +0800

    update

--- projects/views.py
+++ projects/views.py
@@ -7,7 +7,7 @@ import requests
 import json
 
 GITHUB_TOKEN = settings.GITHUB_TOKEN
-
+HEADERS = {"Authorization": f"token {GITHUB_TOKEN}"}
 
 # Create your views here.
 def projects(request):
@@ -15,41 +15,40 @@ def projects(request):
 
     # The name in the link can be changed
     return HttpResponse(
-        template.render({"repos": get_github_repos("YianXie").json()}, request)
+        template.render({"repos": get_github_repos("YianXie")}, request)
     )
 
 
 def get_github_repos(username):
     url = f"https://api.github.com/users/{username}/repos"
-    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=HEADERS)
 
     if response.status_code == 200:
-        return response
+        print('valid')  
+        return response.json()
     else:
+        print(f"invalid status code of {response.status_code}")
         return {"error": f"Failed to fetch repositories ({response.status_code})"}
 
 
 @csrf_exempt
 def get_github_repo_langs(request):
-    if request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            url = data.get("url")
-            headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-
-            response = requests.get(url, headers=headers)
-
-            if response.status_code == 200:
-                return JsonResponse(response.json(), safe=False)
-            else:
-                return JsonResponse(
-                    {"error": "Failed to fetch repositories"}, status=400
-                )
-
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON data"}, status=400)
-
-    else:
+    if request.method != "POST":
         return JsonResponse({"error": f"Only POST method allowed {request.method}"}, status=405)
+    
+    try:
+        data = json.loads(request.body)
+        url = data.get("url")
+
+        response = requests.get(url, headers=HEADERS)
+
+        if response.status_code == 200:
+            return JsonResponse(response.json(), safe=False)
+        else:
+            return JsonResponse(
+                {"error": "Failed to fetch repositories"}, status=400
+            )
+
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON data"}, status=400)
